40921b7bba4f78a2566d5602c3a57c7c945090fa
Nathan Archibald <narchie2013@gmail.com>
2014-04-15 20:53:58 -0600

    1st update

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,23 @@
 
 
 
+angular.module("angular-friends").controller('friendController', function ($scope){
+    $scope.test = "Hello everyone"
+
+    $.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(data){
+       $scope.$apply(function(){
+        $scope.friends= data.results;
+
+
+
+
+       });
+
+
+    });
+
+});
+
+
 
 
