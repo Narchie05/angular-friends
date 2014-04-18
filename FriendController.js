740af8d63bc904e7a825c78458505d15e433e583
Nathan Archibald <narchie2013@gmail.com>
2014-04-17 19:43:44 -0600

    service1

--- FriendController.js
+++ FriendController.js
@@ -1,22 +1,50 @@
+angular.module("angular-friends").service('friendService', function ($http) {
+    return {
+        getFriends: function () {
+            return  $http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json')
+        }
+    }
+});
 
 
 
-angular.module("angular-friends").controller('friendController', function ($scope){
-    $scope.test = "Hello everyone"
 
-    $.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(data){
-       $scope.$apply(function(){
+angular.module("angular-friends").controller('friendController', function ($scope, friendService){
+
+
+
+        friendService.getFriends()
+        .success(function(data){
         $scope.friends= data.results;
 
+        });
+
+               $scope.sortProperties = [
+                      { text: 'Name', value: 'name' },
+                      { text: '# Friends', value: 'friend_count' },
+                      { text: 'City', value: 'current_location.city' },
+                      { text: 'State', value: 'current_location.state' },
+                           { text: 'Country', value: 'current_location.country' }
+                        ]
+                     $scope.sortProperty = $scope.sortProperties[0].value
+
+                     $scope.sortDirections = [
+                       { text: 'Ascending', value: false },
+                           { text: 'Descending', value: true }
+                         ]
+                     $scope.sortDirection = $scope.sortDirections[0].value
+
+                    });
+
+           
 
 
 
-       });
 
 
-    });
 
-});
+
+
 
 
 
